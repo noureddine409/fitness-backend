5a17705b808a226497fb4cb08186ae2a675f98b9
noureddine <noureddinelachgar9@gmail.com>
2023-03-15 01:27:23 +0100

    made dto like model

---
+++ src/main/java/com/metamafitness/fitnessbackend/dto/TrainerDto.java
@@ -0,0 +1,15 @@
+package com.metamafitness.fitnessbackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TrainerDto extends AppUserDto{
+
+    private boolean verified;
+}
