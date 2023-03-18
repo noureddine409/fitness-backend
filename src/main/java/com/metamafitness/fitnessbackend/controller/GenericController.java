7d4918cfc1f431707eaf00f2fa7da331f93c4ac7
noureddine <noureddinelachgar9@gmail.com>
2023-03-18 23:14:01 +0100

    optimize code

--- src/main/java/com/metamafitness/fitnessbackend/controller/GenericController.java
+++ src/main/java/com/metamafitness/fitnessbackend/controller/GenericController.java
@@ -2,7 +2,6 @@ package com.metamafitness.fitnessbackend.controller;
 
 
 import com.metamafitness.fitnessbackend.dto.GenericDto;
-import com.metamafitness.fitnessbackend.exception.ElementAlreadyExistException;
 import com.metamafitness.fitnessbackend.exception.ElementNotFoundException;
 import com.metamafitness.fitnessbackend.model.GenericEntity;
 import com.metamafitness.fitnessbackend.service.GenericService;
@@ -13,7 +12,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -57,29 +57,11 @@ public abstract class GenericController<T extends GenericEntity, D extends Gener
                 .collect(Collectors.toList());
     }
 
-    @PostMapping
-    public ResponseEntity<D> save(@RequestBody D dto) throws ElementAlreadyExistException {
-        T entity = convertToEntity(dto);
-        T savedEntity = genericService.save(entity);
-        return new ResponseEntity<D>(convertToDto(savedEntity), HttpStatus.CREATED);
-    }
 
-    @GetMapping("/")
+    @GetMapping("/{id}")
 
     public ResponseEntity<D> getById(@PathVariable("id") Long id) throws ElementNotFoundException {
         return new ResponseEntity<D>(convertToDto(genericService.findById(id)), HttpStatus.OK);
-    }
-
-    @PutMapping("/{id}")
-    public ResponseEntity<D> update(@PathVariable("id") Long id, @RequestBody D dto) throws ElementNotFoundException {
-        dto.setId(id);
-        T entity = genericService.update(id, convertToUpdateEntity(dto));
-        return new ResponseEntity<>(convertToDto(entity), HttpStatus.OK);
-    }
-
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable("id") Long id) throws ElementNotFoundException {
-        return new ResponseEntity<Boolean>(genericService.delete(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/metamafitness/fitnessbackend/controller/UserController.java
+++ src/main/java/com/metamafitness/fitnessbackend/controller/UserController.java
@@ -1,12 +1,14 @@
 package com.metamafitness.fitnessbackend.controller;
 
 import com.metamafitness.fitnessbackend.dto.UserDto;
-import com.metamafitness.fitnessbackend.exception.ElementAlreadyExistException;
 import com.metamafitness.fitnessbackend.exception.ElementNotFoundException;
 import com.metamafitness.fitnessbackend.model.User;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
@@ -16,28 +18,11 @@ public class UserController extends GenericController<User, UserDto>{
     public ModelMapper getModelMapper() {
         return super.getModelMapper();
     }
-
-    @Override
-    @PostMapping
-    public ResponseEntity<UserDto> save(@RequestBody UserDto dto) throws ElementAlreadyExistException {
-        return super.save(dto);
-    }
-
     @Override
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getById(@PathVariable("id") Long id) throws ElementNotFoundException {
         return super.getById(id);
     }
 
-    @Override
-    @PutMapping("/{id}")
-    public ResponseEntity<UserDto> update(@PathVariable("id") Long id, @RequestBody UserDto dto) throws ElementNotFoundException {
-        return super.update(id, dto);
-    }
 
-    @Override
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Boolean> delete(@PathVariable("id") Long id) throws ElementNotFoundException {
-        return super.delete(id);
-    }
 }
