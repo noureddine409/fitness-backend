ef6a3f4a21464c5a756042dfebdc794dbbdd1f4c
Lachgar Nour Eddine <noureddinelachgar9@gmail.com>
2023-04-18 21:39:40 +0000

    Revert "Merge branch 'hamid' into 'develop'"
    This reverts merge request !36

--- src/main/java/com/metamafitness/fitnessbackend/service/BlogService.java
+++
@@ -1,9 +0,0 @@
-package com.metamafitness.fitnessbackend.service;
-
-import com.metamafitness.fitnessbackend.model.Blog;
-
-import java.util.List;
-
-public interface BlogService extends GenericService<Blog> {
-    List<Blog> findByCreator(Long id, int page, int size);
-}
