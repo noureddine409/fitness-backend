f44804ab53a4b04173b5f2fe045cbaa6ca61d7c7
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2023-04-18 21:54:52 +0200

    blog api created

---
+++ src/main/java/com/metamafitness/fitnessbackend/service/impl/BlogServiceImpl.java
@@ -0,0 +1,24 @@
+package com.metamafitness.fitnessbackend.service.impl;
+
+import com.metamafitness.fitnessbackend.model.Blog;
+import com.metamafitness.fitnessbackend.repository.BlogRepository;
+import com.metamafitness.fitnessbackend.service.BlogService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BlogServiceImpl extends GenericServiceImpl<Blog> implements BlogService {
+    private final BlogRepository blogRepository;
+    public BlogServiceImpl(BlogRepository blogRepository) {
+        this.blogRepository = blogRepository;
+    }
+
+    @Override
+    public List<Blog> findByCreator(Long id, int page, int size) {
+        Pageable pageable = PageRequest.of(page, size);
+        return blogRepository.findByCreatedBy_id(id, pageable);
+    }
+}
