2aaafbe81545432ada8b0ea52102c6d7b472bf05
HamidIdifi <114613662+HamidIdifi@users.noreply.github.com>
2023-05-01 02:03:25 +0200

    search reviews of current trainer

--- src/main/java/com/metamafitness/fitnessbackend/controller/ReviewController.java
+++ src/main/java/com/metamafitness/fitnessbackend/controller/ReviewController.java
@@ -2,6 +2,8 @@ package com.metamafitness.fitnessbackend.controller;
 
 import com.metamafitness.fitnessbackend.dto.ProgramReviewDto;
 import com.metamafitness.fitnessbackend.dto.ReviewPatchDto;
+import com.metamafitness.fitnessbackend.dto.SearchDto;
+import com.metamafitness.fitnessbackend.exception.BusinessException;
 import com.metamafitness.fitnessbackend.exception.ElementAlreadyExistException;
 import com.metamafitness.fitnessbackend.exception.ResourceOwnershipException;
 import com.metamafitness.fitnessbackend.model.Program;
@@ -9,9 +11,15 @@ import com.metamafitness.fitnessbackend.model.ProgramReview;
 import com.metamafitness.fitnessbackend.model.User;
 import com.metamafitness.fitnessbackend.service.ProgramService;
 import com.metamafitness.fitnessbackend.service.ReviewService;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.metamafitness.fitnessbackend.common.CoreConstant.Exception.ALREADY_EXISTS;
 import static com.metamafitness.fitnessbackend.common.CoreConstant.Exception.AUTHORIZATION_RESOURCE_OWNERSHIP;
@@ -68,6 +76,30 @@ public class ReviewController extends GenericController<ProgramReview, ProgramRe
         Boolean deleted = reviewService.delete(reviewId);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).body(deleted);
     }
+    @Override
+    @PostMapping("/search")
+    public ResponseEntity<List<ProgramReviewDto>> search(@RequestBody SearchDto searchDto) throws BusinessException {
+        searchDto.validate();
+        Pageable pageable = PageRequest.of(searchDto.getPage(), searchDto.getSize());
+        List<ProgramReview> reviews = reviewService.search(searchDto.getKeyword(), pageable);
+
+        // Get the current user
+        User currentUser = getCurrentUser();
+
+        List<ProgramReviewDto> dtos = reviews.stream()
+                .filter(review -> review.getProgram().getCreatedBy().equals(currentUser))
+                .map(this::convertToDto)
+                .collect(Collectors.toList());
+
+        long totalElements = reviewService.countAll();
+        int totalPages = (int) Math.ceil((double) totalElements / searchDto.getSize());
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-Total-Pages", String.valueOf(totalPages));
+        headers.add("Access-Control-Expose-Headers", "X-Total-Pages");
+        return ResponseEntity.status(HttpStatus.OK).headers(headers).body(dtos);
+    }
+
 
     private boolean isNotOwner(ProgramReview review) {
         final Long currentUserId = getCurrentUserId();
